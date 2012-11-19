142bb6ef7b88887172c52510cead5a61c0b9d352
Bikram Chatterjee <razorpeak@gmail.com>
2012-11-19 15:53:22 +0100

    exception handling on local interface

--- src/erlimem_cmds.erl
+++ src/erlimem_cmds.erl
@@ -6,14 +6,28 @@ exec(Cmd, {tcp, Socket}) ->
     gen_tcp:send(Socket, term_to_binary(Cmd)),
     recv_term(Socket, <<>>);
 exec(CmdTuple, {rpc, Node}) ->
-    {Cmd, Args} = lists:split(1, tuple_to_list(CmdTuple)),
-    rpc:call(Node, imem_sec, lists:nth(1, Cmd), Args);
+    exec_catch(Node, imem_sec, CmdTuple);
 exec(CmdTuple, {local_sec, _}) ->
-    {Cmd, Args} = lists:split(1, tuple_to_list(CmdTuple)),
-    apply(imem_sec, lists:nth(1, Cmd), Args);
+    exec_catch(node(), imem_sec, CmdTuple);
 exec(CmdTuple, {local, _}) ->
+    exec_catch(node(), imem_meta, CmdTuple).
+
+exec_catch(Node, Mod, CmdTuple) when Node =:= node() ->
     {Cmd, Args} = lists:split(1, tuple_to_list(CmdTuple)),
-    apply(imem_meta, lists:nth(1, Cmd), Args).
+    Fun = lists:nth(1, Cmd),
+    try
+        apply(Mod, Fun, Args)
+    catch
+        _Class:Result -> {error, Result}
+    end;
+exec_catch(Node, Mod, CmdTuple) ->
+    {Cmd, Args} = lists:split(1, tuple_to_list(CmdTuple)),
+    Fun = lists:nth(1, Cmd),
+    try
+        rpc:call(Node, Mod, Fun, Args)
+    catch
+        _Class:Result -> {error, Result}
+    end.
 
 recv_term(Sock, Bin) ->
     case gen_tcp:recv(Sock, 0) of
