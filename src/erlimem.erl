18742d33b63648c3b8c904ef42113bc5394e9394
Agustin Cautin <acautin@gmail.com>
2013-07-04 21:27:15 +0200

    Using lager as logger tool for erlimem.

--- src/erlimem.erl
+++ src/erlimem.erl
@@ -11,7 +11,23 @@
 
 loglevel(L) -> application:set_env(erlimem, logging, L).
 
-start() ->  application:start(?MODULE).
+start() ->
+    ok = application:load(lager),
+    ok = application:set_env(lager, handlers, [{lager_console_backend, info},
+                                               {lager_file_backend, [{file, "log/error.log"},
+                                                                     {level, error},
+                                                                     {size, 10485760},
+                                                                     {date, "$D0"},
+                                                                     {count, 5}]},
+                                               {lager_file_backend, [{file, "log/console.log"},
+                                                                     {level, info},
+                                                                     {size, 10485760},
+                                                                     {date, "$D0"},
+                                                                     {count, 5}]}]),
+    ok = application:set_env(lager, error_logger_redirect, false),
+    ok = application:start(lager),
+    application:start(?MODULE).
+
 stop()  ->  application:stop(?MODULE).
 
 open(Type, Opts, Cred) ->
--- src/erlimem.hrl
+++ src/erlimem.hrl
@@ -2,34 +2,18 @@
 -define(SESSION_TIMEOUT, 3600000).
 -define(MAX_PREFETCH_ON_FLIGHT, 5).
 
--include("log.hrl").
 -define(LOG_TAG, "_IMDR_").
 
--define(Debug(__M,__F,__A), ?LOG(?LOG_TAG, dbg, __M, "{~p,~4..0B} "++__F, [?MODULE,?LINE]++__A)).
--define(Debug(__F,__A),     ?LOG(?LOG_TAG, dbg,  [], "{~p,~4..0B} "++__F, [?MODULE,?LINE]++__A)).
--define(Debug(__F),         ?LOG(?LOG_TAG, dbg,  [], "{~p,~4..0B} "++__F, [?MODULE,?LINE])).
+-define(Debug(__M,__F,__A), lager:debug(__M, "["++?LOG_TAG++"] ~p "++__F, [{?MODULE,?LINE}]++__A)).
+-define(Debug(__F,__A),     lager:debug(     "["++?LOG_TAG++"] ~p "++__F, [{?MODULE,?LINE}]++__A)).
+-define(Debug(__F),         lager:debug(     "["++?LOG_TAG++"] ~p "++__F, [{?MODULE,?LINE}])).
 
--define(Info(__M,__F,__A),  ?LOG(?LOG_TAG, nfo, __M, "{~p,~4..0B} "++__F, [?MODULE,?LINE]++__A)).
--define(Info(__F,__A),      ?LOG(?LOG_TAG, nfo,  [], "{~p,~4..0B} "++__F, [?MODULE,?LINE]++__A)).
--define(Info(__F),          ?LOG(?LOG_TAG, nfo,  [], "{~p,~4..0B} "++__F, [?MODULE,?LINE])).
+-define(Info(__M,__F,__A),  lager:info(__M,  "["++?LOG_TAG++"] ~p "++__F, [{?MODULE,?LINE}]++__A)).
+-define(Info(__F,__A),      lager:info(      "["++?LOG_TAG++"] ~p "++__F, [{?MODULE,?LINE}]++__A)).
+-define(Info(__F),          lager:info(      "["++?LOG_TAG++"] ~p "++__F, [{?MODULE,?LINE}])).
 
--define(Error(__M,__F,__A), ?LOG(?LOG_TAG, err, __M, "{~p,~4..0B} "++__F, [?MODULE,?LINE]++__A)).
--define(Error(__F,__A),     ?LOG(?LOG_TAG, err,  [], "{~p,~4..0B} "++__F, [?MODULE,?LINE]++__A)).
--define(Error(__F),         ?LOG(?LOG_TAG, err,  [], "{~p,~4..0B} "++__F, [?MODULE,?LINE])).
+-define(Error(__M,__F,__A), lager:error(__M, "["++?LOG_TAG++"] ~p "++__F, [{?MODULE,?LINE}]++__A)).
+-define(Error(__F,__A),     lager:error(     "["++?LOG_TAG++"] ~p "++__F, [{?MODULE,?LINE}]++__A)).
+-define(Error(__F),         lager:error(     "["++?LOG_TAG++"] ~p "++__F, [{?MODULE,?LINE}])).
 
 -define(NoRefSqlRegEx, "^(?i)(CREATE|INSERT|UPDATE|DELETE|DROP)").
-
--record(fsmctx, { % fsm interface
-                  id
-                , stmtColsLen
-                , rowFun
-                , sortFun
-                , sortSpec
-                , block_length
-                , fetch_recs_async_fun
-                , fetch_close_fun
-                , filter_and_sort_fun
-                , update_cursor_prepare_fun
-                , update_cursor_execute_fun
-                }).
-
