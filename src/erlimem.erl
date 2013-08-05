1085d064f33637cc561602eb50042d9a0579f38d
Bikram Chatterjee <razorpeak@gmail.com>
2013-08-05 10:58:08 +0200

    rowfin generator mode reload disabled

--- src/erlimem.erl
+++ src/erlimem.erl
@@ -37,23 +37,24 @@ open(Type, Opts, Cred) ->
     end,
     case gen_server:start(?SESSMOD, [Type, Opts, Cred], []) of
         {ok, Pid} ->
-            Sess = {?SESSMOD, Pid},
-            LocalVsn = proplists:get_value(vsn,imem_datatype:module_info(attributes)),
-            RemoteVsn = proplists:get_value(vsn, Sess:run_cmd(admin_exec, [imem_datatype,module_info,[attributes]])),
-            if LocalVsn =/= RemoteVsn ->
-                    try
-                        {imem_datatype,Bin,File} = Sess:run_cmd(admin_exec, [code,get_object_code,[imem_datatype]]),
-                        {module, imem_datatype} = code:load_binary(imem_datatype, File, Bin),
-                        ?Info("imem_datatype reloaded old vsn ~p new vsn ~p", [LocalVsn, RemoteVsn]),
-                        {ok, {?SESSMOD, Pid}}
-                    catch
-                        _:Reason ->
-                            ?Error("error loading compatible version of imem_datatype ~p", [Reason]),
-                            {error, Reason}
-                    end;
-                true ->
-                    {ok, {?SESSMOD, Pid}}
-            end;
+            %Sess = {?SESSMOD, Pid},
+            %LocalVsn = proplists:get_value(vsn,imem_datatype:module_info(attributes)),
+            %RemoteVsn = proplists:get_value(vsn, Sess:run_cmd(admin_exec, [imem_datatype,module_info,[attributes]])),
+            %if LocalVsn =/= RemoteVsn ->
+            %        try
+            %            {imem_datatype,Bin,File} = Sess:run_cmd(admin_exec, [code,get_object_code,[imem_datatype]]),
+            %            {module, imem_datatype} = code:load_binary(imem_datatype, File, Bin),
+            %            ?Info("imem_datatype reloaded old vsn ~p new vsn ~p", [LocalVsn, RemoteVsn]),
+            %            {ok, {?SESSMOD, Pid}}
+            %        catch
+            %            _:Reason ->
+            %                ?Error("error loading compatible version of imem_datatype ~p", [Reason]),
+            %                {error, Reason}
+            %        end;
+            %    true ->
+            %        {ok, {?SESSMOD, Pid}}
+            %end;
+            {ok, {?SESSMOD, Pid}};
         {error, {error, Error}} -> {error, Error};
         Other -> Other
     end.
